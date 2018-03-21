c3e0241d14c1beb85a37e32fa3dbfd2a308f37b3
Alvis Duong <alvisduong@gmail.com>
2018-03-21 16:39:02 +0000

    Give Product a quantity attribute

--- test/product.rb
+++ test/product.rb
@@ -3,11 +3,12 @@ require 'minitest/autorun'
 
 describe Product do
   before do
-    @product = Product.new('Kinder Egg', 99)
+    @product = Product.new('Kinder Egg', 99, 200)
   end
 
-  it 'has name and price' do
+  it 'has name, price and quantity' do
     @product.name.must_equal('Kinder Egg')
     @product.price.must_equal(99)
+    @product.quantity.must_equal(200)
   end
 end
